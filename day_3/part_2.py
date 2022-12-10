cc1df59e00e82dfa3203348fef30f2317874ec99
Matt Smith <matthew.smith@aligent.com.au>
2022-12-10 20:26:43 +0000

    Add Day 3 Part 2 solution

---
+++ day_3/part_2.py
@@ -0,0 +1,54 @@
+import string
+import shared
+
+class Solution:
+    """ Solution for Day 3 Part 2
+    """
+
+    def __init__(self, debug):
+        self.debug = debug
+
+        self.input_file = 'data.txt'
+        if debug:
+            self.input_file = 'debug_data.txt'
+
+    def get_priority(self, item):
+        """uses the position of the letter within the alphabet to determine a priority
+
+        Args:
+            item (_type_): the letter of the alphabet for analysis
+
+        Returns:
+            integer: the priority
+        """
+        return string.ascii_letters.index(item) + 1
+
+    def run(self):
+        """all the elven magic happens here
+        """
+        rucksacks = priority_total = 0
+        group = []
+
+        rucksack_data = shared.fileToArray('day_3/'+self.input_file)
+
+        for contents in rucksack_data:
+            rucksacks += 1
+
+            # if we keep on intersecting, by the time we get to round 3 there should be only one
+            if not group:
+                group = set(contents)
+            else:
+                group = set(contents).intersection(group)
+
+            if rucksacks % 3 == 0:
+                item = group.pop()
+
+                priority = self.get_priority(item)
+                if self.debug:
+                    print(f'item: {item} | {priority}')
+
+                group = []
+                priority_total += priority
+
+        print(f'Total rucksacks: {rucksacks}')
+        print(f'Total p total: {priority_total}')
