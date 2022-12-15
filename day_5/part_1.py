bf9f8d6bece6f2427f78d14c846eb81e1500279e
hellboy1975 <hellboy1975@gmail.com>
2022-12-15 22:00:32 +1030

    add early Day1 Part 1 prototype

--- day_5/part_1.py
+++ day_5/part_1.py
@@ -1,54 +1,66 @@
 import shared
 from shared import bcolours
+from shared import baseSolution
+import re
 
-class Solution:
-
-    def __init__(self, debug):
-        self.debug = debug
-
-        self.inputFile = 'data.txt'
-        if (debug):
-            self.inputFile = 'debug_data.txt'
+class Solution(baseSolution):
+    MOVE = 0
+    FROM = 1
+    TO = 2
 
     def get_start_warehouse(self):
         
 
         if (self.debug):
-            warehouse = {
-                "1": ['Z','N'],
-                "2": ['M','C','D'],
-                "3": ['P']
-            }
+            warehouse = [
+                ['Z','N'],
+                ['M','C','D'],
+                ['P']
+            ]
 
         else:
-            warehouse = {
-                "1": ['Z','J','N','W','P','S'],
-                "2": ['G','S','T'],
-                "3": ['V','Q','R','L','H'],
-                "4": ['V','S','T','D'],
-                "5": ['Q','Z','T','D','B','M','J'],
-                "6": ['M','W','T','J','D','C','Z','L'],
-                "7": ['L','P','M','W','G','T','J'],
-                "8": ['N','G','M','T','B','F','Q','H'],
-                "9": ['R','D','G','C','P','B','Q','W']
-            }
+            warehouse = [
+                ['Z','J','N','W','P','S'],
+                ['G','S','T'],
+                ['V','Q','R','L','H'],
+                ['V','S','T','D'],
+                ['Q','Z','T','D','B','M','J'],
+                ['M','W','T','J','D','C','Z','L'],
+                ['L','P','M','W','G','T','J'],
+                ['N','G','M','T','B','F','Q','H'],
+                ['R','D','G','C','P','B','Q','W']
+            ]
 
         return warehouse
 
     def print_crate(self, code):
         print(code,end='')
 
+    def apply_instruction(self, instruction, warehouse):
+        num_moves = instruction[self.MOVE]
+        print(f'Num of items to move: {num_moves}')
+        print(f'warehouse: {warehouse}')
+        for i in range(1, num_moves):
+            crate = warehouse[instruction[self.FROM] - 1].pop()
+            warehouse[instruction[self.TO] - 1].append(crate)
+            i += 1
+
+        return warehouse
+
+
+    def split_instruction(self, instruction):
+        # sample move 1 from 3 to 5
+        print(instruction)
+        return [int(s) for s in re.findall(r'\b\d+\b', instruction)]
+
     def display_warehouse(self, warehouse):
-        print(warehouse)
         sc = 0
         for stack in warehouse:
             sc += 1
-            print(stack)
-            # print(f'{sc}:', end='')
+            print(f'{sc}:', end='')
             for crate in stack:
-                print(crate)
-                # self.print_crate(crate)
-            # print('')
+                self.print_crate(crate)
+            print('')
 
 
     def run(self):
@@ -60,10 +72,18 @@ class Solution:
 
         warehouse = self.get_start_warehouse()
 
+        print('Start Warehouse:')
         self.display_warehouse(warehouse)
 
         for move in move_data:
             moves += 1
+            self.apply_instruction(self.split_instruction(move), warehouse)
+            if self.visualise:
+                print(f'\n After move {moves}')
+                self.display_warehouse(warehouse)
+            
 
+        print('Final Warehouse:')
+        self.display_warehouse(warehouse)
 
         print(f'Total moves: {moves}')
